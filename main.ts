ca55d31e1453c643684e8f0d4d858a877f6dea74
Paulalejandraa <81317382+Paulalejandraa@users.noreply.github.com>
2021-05-14 22:03:20 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    p1 += pins.map(
+    input.temperature(),
+    0,
+    1023,
+    0,
+    100
+    )
+    basic.showString("" + (p1))
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + ((p1 + p2 + p3) / 3))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (p1))
+    basic.pause(1000)
+    basic.showString("" + (p2))
+    basic.pause(1000)
+    basic.showString("" + (p3))
+    basic.pause(1000)
+})
+let p3 = 0
+let p2 = 0
+let p1 = 0
+p1 = 0
+p2 = 0
+p3 = 0
