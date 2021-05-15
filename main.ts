34cd862212f7f349dddcced774d4c2561415cba0
Paulalejandraa <81317382+Paulalejandraa@users.noreply.github.com>
2021-05-14 22:38:06 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,41 @@
 input.onButtonPressed(Button.A, function () {
-    p1 += pins.map(
-    input.temperature(),
+    temperatura = pins.analogReadPin(AnalogPin.P0)
+    p1 = pins.map(
+    temperatura,
     0,
     1023,
     0,
     100
     )
-    basic.showString("" + (p1))
+    temperatura = pins.analogReadPin(AnalogPin.P1)
+    p2 = pins.map(
+    temperatura,
+    0,
+    1023,
+    0,
+    100
+    )
+    temperatura = pins.analogReadPin(AnalogPin.P2)
+    p3 = pins.map(
+    temperatura,
+    0,
+    1023,
+    0,
+    100
+    )
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("" + ((p1 + p2 + p3) / 3))
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("" + (p1))
-    basic.pause(1000)
+    basic.pause(500)
     basic.showString("" + (p2))
-    basic.pause(1000)
+    basic.pause(500)
     basic.showString("" + (p3))
-    basic.pause(1000)
+    basic.pause(500)
 })
+let temperatura = 0
 let p3 = 0
 let p2 = 0
 let p1 = 0
